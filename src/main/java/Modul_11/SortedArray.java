60898f3ad8a9e44d7e67d5d07bdc6f71b96e3206
Bogdan Sinyavskiy <sinavskijb@gmail.com>
2023-03-03 14:25:09 +0100

    completed sorted by A-Z

---
+++ src/main/java/Modul_11/SortedArray.java
@@ -0,0 +1,16 @@
+package Modul_11;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class SortedArray {
+    public static void main(String[] args) {
+        Stream<String> nameStream;
+        nameStream = Stream.of("Ivan", "Kristina", "OLeg", "Artem", "Peter", "Julia")
+                .map(name -> name.toUpperCase())
+                .sorted();
+        List<String> names = nameStream.collect(Collectors.toList());
+        System.out.println(names);
+    }
+}
