c2e40fe083c0b2de0ad9724538890915d6b181e9
Bogdan Sinyavskiy <sinavskijb@gmail.com>
2023-03-05 21:45:24 +0100

    Module 12 homework 2 by ExecutorService, second try)

--- src/main/java/Module_12/FizzBuzzOrFizzbuzz.java
+++ src/main/java/Module_12/FizzBuzzOrFizzbuzz.java
@@ -1,44 +1,29 @@
 package Module_12;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class FizzBuzzOrFizzbuzz {
+
+    private final int n = 20;
+    public static volatile AtomicInteger digits = new AtomicInteger(1);
+    public BlockingQueue<String> queue = new LinkedBlockingQueue<>();
     public static void main(String[] args) throws InterruptedException {
         ExecutorService executorService = Executors.newScheduledThreadPool(4);
-        ThreadServiceMethods threadServiceMethods = new ThreadServiceMethods(1);
-       while (threadServiceMethods.getDigits() > 0){
-           executorService.submit(() -> {
-               try {
-                   threadServiceMethods.fizz();
-               } catch (InterruptedException e) {
-                   throw new RuntimeException(e);
-               }
-           });
-           executorService.submit(()-> {
-               try {
-                   threadServiceMethods.buzz();
-               } catch (InterruptedException e) {
-                   throw new RuntimeException(e);
-               }
-           });
-           executorService.submit(()->{
-               try {
-                   threadServiceMethods.fizzbuzz();
-               } catch (InterruptedException e) {
-                   throw new RuntimeException(e);
-               }
-           });
-           executorService.submit(()-> {
-               try {
-                   threadServiceMethods.number();
-               } catch (InterruptedException e) {
-                   throw new RuntimeException(e);
-               }
-           });
-       }
-
-
+        ThreadServiceMethods threadServiceMethods = new ThreadServiceMethods();
+        threadServiceMethods.setN(20);
+        executorService.submit(threadServiceMethods::fizz);
+        executorService.submit(threadServiceMethods::buzz);
+        executorService.submit(threadServiceMethods::fizzbuzz);
+        executorService.submit(threadServiceMethods::number);
+        executorService.submit(threadServiceMethods::print);
+        executorService.shutdown();
     }
 
+
 }
+
+
--- src/main/java/Module_12/ThreadServiceMethods.java
+++ src/main/java/Module_12/ThreadServiceMethods.java
@@ -1,65 +1,112 @@
 package Module_12;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.lang.Thread.sleep;
+
 public class ThreadServiceMethods {
-    public int getDigits() {
-        return digits;
+
+    public static volatile AtomicInteger digits = new AtomicInteger(1);
+    public BlockingQueue<String> queue = new LinkedBlockingQueue<>();
+
+    private int n = 20;
+
+    public synchronized void add(String element) {
+        queue.add(element);
     }
 
-    private int digits;
-
-    public ThreadServiceMethods(int digits) {
-        this.digits = digits;
+    public void setN(int n) {
+        this.n = n;
     }
 
-    public synchronized void fizz() throws InterruptedException {
-        while (digits < 21) {
-            if (digits % 3 == 0 && digits % 5 != 0) {
-                System.out.println("fizz");
-                Thread.currentThread().notifyAll();
+    public synchronized void fizz()  {
+        while (digits.get() < 21) {
+            if (digits.get() % 3 == 0 && digits.get() % 5 != 0) {
+                add("fizz");
+                digits.getAndIncrement();
+                notifyAll();
             } else {
-                Thread.currentThread().wait();
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
             }
-            digits++;
         }
 
     }
 
-    public synchronized void buzz() throws InterruptedException {
-        while (digits < 21) {
-            if (digits % 5 == 0 && digits % 3 != 0) {
-                System.out.println("buzz");
-                Thread.currentThread().notifyAll();
+    public synchronized void buzz()  {
+        while (digits.get() < 21) {
+            if (digits.get() % 5 == 0 && digits.get() % 3 != 0) {
+                add("buzz");
+                digits.getAndIncrement();
+                notifyAll();
             } else {
-                Thread.currentThread().wait();
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
-            digits++;
 
         }
     }
 
-    public synchronized void fizzbuzz() throws InterruptedException {
-        while (digits < 21) {
-            if (digits % 15 == 0) {
-                System.out.println("fizzbuzz");
-                Thread.currentThread().notifyAll();
+    public synchronized void fizzbuzz()  {
+        while (digits.get() < 21) {
+            if (digits.get() % 15 == 0) {
+                add("fizzbuzz");
+                digits.getAndIncrement();
+                notifyAll();
             } else {
-                Thread.currentThread().wait();
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
-            digits++;
 
         }
     }
 
-    public synchronized void number() throws InterruptedException {
-        while (digits < 21) {
-            if (digits % 3 != 0 && digits % 5 != 0) {
-                System.out.println(digits);
-                Thread.currentThread().notifyAll();
+    public synchronized void number() {
+        while (digits.get() < 21) {
+            if (digits.get() % 3 != 0 && digits.get() % 5 != 0) {
+                add(String.valueOf(digits));
+                digits.getAndIncrement();
+                notifyAll();
             } else {
-                Thread.currentThread().wait();
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
-            digits++;
 
+        }
+    }
+
+    public void print() {
+        while (true) {
+
+            while (!queue.isEmpty()) {
+
+                try {
+                    sleep(300);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                if (queue.size() > 1)
+                    System.out.print(queue.poll() + ", ");
+                else System.out.print(queue.poll());
+
+            }
+            notifyAll();
         }
     }
 }
